a1e86c9c935a962bedbd4dd94da5d84cac381030
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2016-10-12 21:35:34 +0200

    Update create-xaview.sh

--- oracle/create-xaview.sh
+++ oracle/create-xaview.sh
@@ -6,4 +6,4 @@ sqlplus sys/oracle as sysdba @"$ORACLE_HOME/rdbms/admin/xaview.sql"
 echo "creating database users with xa required grants"
 sqlplus sys/oracle as sysdba @"/docker-entrypoint-initdb.d/sysdba/init-databases.sql"
 
-echo "end of customization"
+echo "end of customization."
